6bce351e40a684f9696c4fb33a5a328ab68c806a
slay <haras.lay@gmail.com>
2021-10-25 17:57:37 +0100

    delete ligne card 5

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,12 +2,11 @@
 
 import React from "react";
 import "./Cards.css";
-import image from "./bibliotete.jpg";
 
 const Cards = (props) => {
   return (
     <div className='myCard'>
-      {/*  src={`https://robohash.org/${id}`} alt=''></img> */}
+     
       <img className='imageRobot' src={props.src} alt=''></img>
       <div className='detailsRobots'>
         <h2>{props.nom}</h2>
